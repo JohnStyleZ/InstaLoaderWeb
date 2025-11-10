a3cc63cb1bc13c19f9576917837dc041ce8af7c3
John Wu <33677676+JohnStyleZ@users.noreply.github.com>
2025-11-10 15:44:35 -0800

    Update urls.py

--- InstaLoaderWeb/urls.py
+++ InstaLoaderWeb/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("downloader.urls")),
+    path("", include("downloader.urls")),  # your app routes
 ]
+
+# Dev/static serving for media (OK on Render while you get S3/Disk set up)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
